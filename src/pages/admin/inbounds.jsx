32754cb2029dac96835c5992b1c31c14527ebbd9
Jinse2092 <96506961+Jinse2092@users.noreply.github.com>
2025-05-24 17:53:34 +0530

    ok

--- src/pages/admin/inbounds.jsx
+++ src/pages/admin/inbounds.jsx
@@ -89,7 +89,7 @@ const TestAdminInbounds = () => {
 
   return (
     <div className="p-2 sm:p-6 space-y-6">
-      <h1 className="text-3xl font-bold mb-4">Inbound / Outbound Inventory</h1>
+      <h1 className="text-3xl font-bold mb-4">testing Inbound / Outbound Inventory</h1>
       <Card>
         <CardHeader>
           <input
